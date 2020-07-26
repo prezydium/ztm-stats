b0b3286c71fc240991a9080d13acbfa3a2b60c92
Krzysztof Syska <prezydium834@gmail.com>
2020-07-26 09:49:42 +0200

    Added DataStorage date parsing

--- src/main/java/com/sda/kristoff/ztmstats/core/DataStorage.java
+++ src/main/java/com/sda/kristoff/ztmstats/core/DataStorage.java
@@ -4,15 +4,18 @@ import com.sda.kristoff.ztmstats.model.Vehicle;
 import com.sda.kristoff.ztmstats.model.ZtmData;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class DataStorage {
+
+    private static final DateTimeFormatter DATE_TIME_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd' 'HH:mm:ss");
 
     private LocalDateTime updateTime;
     private List<Vehicle> vehicles;
 
     public void update(ZtmData ztmData) {
-        updateTime = LocalDateTime.parse(ztmData.getUpdateTime());
+        updateTime = LocalDateTime.parse(ztmData.getUpdateTime(), DATE_TIME_FORMAT);
         vehicles = ztmData.getVehicles();
     }
 
--- src/test/java/com/sda/kristoff/ztmstats/core/DataStorageTest.java
+++ src/test/java/com/sda/kristoff/ztmstats/core/DataStorageTest.java
@@ -2,14 +2,15 @@ package com.sda.kristoff.ztmstats.core;
 
 import com.sda.kristoff.ztmstats.inbound.DataMapper;
 import com.sda.kristoff.ztmstats.model.ZtmData;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.time.LocalDateTime;
 
 public class DataStorageTest {
-
 
     @Test
     public void shouldUpdateData() throws IOException {
@@ -19,6 +20,12 @@ public class DataStorageTest {
         ZtmData ztmData = DataMapper.mapJsonToZtmData(testJson);
         //when
         DataStorage dataStorage = new DataStorage();
-    }
+        dataStorage.update(ztmData);
+        //then
+        LocalDateTime expectedDateTime = LocalDateTime.of(2020, 7, 25, 14, 45, 00);
+        LocalDateTime actualUpdateTime = dataStorage.getUpdateTime();
 
+        Assert.assertEquals(expectedDateTime, actualUpdateTime);
+        Assert.assertEquals(7, dataStorage.getVehicles().size());
+    }
 }
