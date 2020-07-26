b075ae6377d5657183be23f60c3646579475c053
Krzysztof Syska <prezydium834@gmail.com>
2020-07-26 12:31:48 +0200

    Added calculator logic and tests

--- src/test/java/com/sda/kristoff/ztmstats/core/CalculatorTest.java
+++ src/test/java/com/sda/kristoff/ztmstats/core/CalculatorTest.java
@@ -3,7 +3,9 @@ package com.sda.kristoff.ztmstats.core;
 import com.sda.kristoff.ztmstats.model.Vehicle;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
@@ -20,12 +22,32 @@ public class CalculatorTest {
 
     @Test
     public void shouldCalculateAvgForAll() {
-
         Double result = Calculator.avgSpeed(testVehicles);
-
-        System.out.println();
-
         assertEquals(24.0, result, 0);
+    }
+
+    @Test
+    public void shouldCalculateAvgForAllLines() {
+        Map<String, Double> result = Calculator.avgSpeedByAllLines(testVehicles);
+        Double avg148 = result.get("148");
+        assertEquals(47.5, avg148, 0);
+    }
+
+    @Test
+    public void shouldCalculateForEmptyList() {
+        Map<String, Double> result = Calculator.avgSpeedByAllLines(new ArrayList<>());
+        assertEquals(0, result.keySet().size());
+    }
+
+    @Test
+    public void shouldCalculateForGivenLine() {
+        Double resultFor148 = Calculator.avgSpeedForGivenLine("148", testVehicles);
+        Double resultFor127 = Calculator.avgSpeedForGivenLine("127", testVehicles);
+        Double resultForNotExistingLine = Calculator.avgSpeedForGivenLine("XXX", testVehicles);
+
+        assertEquals(47.5, resultFor148, 0);
+        assertEquals(0, resultFor127, 0);
+        assertEquals(0, resultForNotExistingLine, 0);
     }
 
 }
