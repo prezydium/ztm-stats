cf317afb11367af956038070dd3986293117d002
Krzysztof Syska <prezydium834@gmail.com>
2020-07-26 09:04:23 +0200

    Added loading test data rom file

--- src/test/java/com/sda/kristoff/ztmstats/inbound/DataMapperTest.java
+++ src/test/java/com/sda/kristoff/ztmstats/inbound/DataMapperTest.java
@@ -4,20 +4,24 @@ import com.sda.kristoff.ztmstats.model.Vehicle;
 import com.sda.kristoff.ztmstats.model.ZtmData;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class DataMapperTest {
 
-    private String testJson = RawDataProviderUtil.TEST_JSON;
-
     @Test
-    public void shouldMapSuccessfully() {
-
+    public void shouldMapSuccessfully() throws IOException {
+        //given
+        InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream("test.json");
+        String testJson = new String(resourceAsStream.readAllBytes(), Charset.defaultCharset());
+        //when
         ZtmData ztmData = DataMapper.mapJsonToZtmData(testJson);
         List<Vehicle> vehicles = ztmData.getVehicles();
-
+        //then
         assertEquals(7, vehicles.size());
         assertEquals("118", vehicles.get(1).getLine());
     }
